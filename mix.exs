e82b37752246f128c30303ea7bbf89827ec99279
smtamay <stamay@ixmatix.com>
2024-01-21 17:39:17 -0600

    Installed Absinthe and related dependencies
    Added the following dependencies to support GraphQL using Absinthe:
    - :absinthe
    - :absinthe_plug
    - :absinthe_phoenix
    - :dataloader
    - :cors_plug
    - :hackney.

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule StreamingBackend.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
@@ -50,7 +51,13 @@ defmodule StreamingBackend.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe_phoenix, "~> 2.0.1"},
+      {:dataloader, "~> 1.0.0"},
+      {:cors_plug, "~> 3.0"},
+      {:hackney, "~> 1.18"}
     ]
   end
 
