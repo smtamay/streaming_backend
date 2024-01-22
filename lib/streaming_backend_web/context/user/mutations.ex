efec6872cfe442357045001614f382224e708d79
smtamay <stamay@ixmatix.com>
2024-01-22 12:02:27 -0600

    feta: Add signin mutation to the User schema.

--- lib/streaming_backend_web/context/user/mutations.ex
+++ lib/streaming_backend_web/context/user/mutations.ex
@@ -2,13 +2,20 @@ defmodule StreamingBackendWeb.Context.User.Mutations do
   alias StreamingBackendWeb.Resolvers.UserResolver
   use Absinthe.Schema.Notation
 
-    object :signup do
-        @desc "Create a user account"
-        field :signup, :session do
-          arg :email, non_null(:string)
-          arg :username, non_null(:string)
-          arg :password, non_null(:string)
-          resolve &UserResolver.signup/3
-        end
+  object :user_mutations do
+    @desc "Create a user account"
+    field :signup, :session do
+      arg(:email, non_null(:string))
+      arg(:username, non_null(:string))
+      arg(:password, non_null(:string))
+      resolve(&UserResolver.signup/3)
     end
+
+    @desc "Sign in a user"
+    field :signin, :session do
+      arg(:email, :string)
+      arg(:password, :string)
+      resolve(&UserResolver.signin/3)
+    end
+  end
 end
