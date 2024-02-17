aae3b0d1ea452d279f87faa1a831e686ecc8aa75
smtamay <stamay@ixmatix.com>
2024-02-17 15:16:05 -0600

    chore: Add migration for creating streaming accounts table
    Added a migration file for creating the 'streaming_accounts' table, which includes fields such as 'streaming_plataform_id', 'price', 'email', 'password', and 'payment_date'.

--- priv/repo/migrations/20240123024035_create_streaming_accounts.exs
+++ priv/repo/migrations/20240123024035_create_streaming_accounts.exs
@@ -3,7 +3,7 @@ defmodule StreamingBackend.Repo.Migrations.CreateStreamingAccounts do
 
   def change do
     create table(:streaming_accounts) do
-      add :streaming_platform_id, references(:streaming_plataforms, on_delete: :nothing)
+      add :streaming_plataform_id, references(:streaming_plataforms, on_delete: :nothing)
       add :price, :float
       add :email, :string
       add :password, :string
@@ -12,6 +12,6 @@ defmodule StreamingBackend.Repo.Migrations.CreateStreamingAccounts do
       timestamps(type: :utc_datetime)
     end
 
-    create index(:streaming_accounts, [:streaming_platform_id])
+    create index(:streaming_accounts, [:streaming_plataform_id])
   end
 end
