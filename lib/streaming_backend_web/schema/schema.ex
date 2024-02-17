30e731b76b06cbca0b56df6393083dabb298118b
smtamay <stamay@ixmatix.com>
2024-02-17 15:24:07 -0600

    chore: Update Absinthe schema with UserSubscriptionInfo queries and mutations

--- lib/streaming_backend_web/schema/schema.ex
+++ lib/streaming_backend_web/schema/schema.ex
@@ -5,16 +5,19 @@ defmodule StreamingBackendWeb.Schema.Schema do
   import_types StreamingBackendWeb.Context.User.Types
   import_types StreamingBackendWeb.Context.StreamingPlataform.Types
   import_types StreamingBackendWeb.Context.StreamingAccount.Types
+  import_types StreamingBackendWeb.Context.UserSubscriptionInfo.Types
 
   # Import queries
   import_types StreamingBackendWeb.Context.User.Queries
   import_types StreamingBackendWeb.Context.StreamingPlataform.Queries
   import_types StreamingBackendWeb.Context.StreamingAccount.Queries
+  import_types StreamingBackendWeb.Context.UserSubscriptionInfo.Queries
 
   # Import mutations
   import_types StreamingBackendWeb.Context.User.Mutations
   import_types StreamingBackendWeb.Context.StreamingPlataform.Mutations
   import_types StreamingBackendWeb.Context.StreamingAccount.Mutations
+  import_types StreamingBackendWeb.Context.UserSubscriptionInfo.Mutations
 
   query do
    import_fields :user_queries
@@ -22,6 +25,8 @@ defmodule StreamingBackendWeb.Schema.Schema do
    import_fields :streaming_plataform_queries
 
    import_fields :streaming_account_queries
+
+   import_fields :queries_subscription_info
   end
 
 
@@ -31,5 +36,7 @@ defmodule StreamingBackendWeb.Schema.Schema do
    import_fields :streaming_plataform_mutations
 
    import_fields :streaming_account_mutations
+
+   import_fields :mutations_user_subscription_info
   end
 end
